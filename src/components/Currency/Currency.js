1313c377d75e6de83ea0a40278e18fc1f25f33a0
adarsh25t <adarsh25t@gmail.com>
2022-04-02 11:18:46 +0530

    add the currency item style

--- src/components/Currency/Currency.js
+++ src/components/Currency/Currency.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './currency.css'
+import CurrencyItem from './CurrencyItem';
 
 function Currency() {
   return (
     <div className='currency'>
-        
+        <CurrencyItem/>
+        <CurrencyItem/>
+        <CurrencyItem/>
     </div>
   )
 }
