e319f80b3f535bfe14878c713908af58ad46ad98
adarsh25t <adarsh25t@gmail.com>
2022-04-02 11:31:33 +0530

    create the add button

---
+++ src/components/AddCurrency/AddCurrency.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './addcurrency.css';
+
+
+function AddCurrency() {
+  return (
+    <div>
+        <button>+ Add a Cryptocurrency</button>
+    </div>
+  )
+}
+
+export default AddCurrency
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import AddCurrency from '../AddCurrency/AddCurrency'
 import Currency from '../Currency/Currency'
 import './home.css'
 
@@ -6,6 +7,7 @@ function Home() {
   return (
     <div className='currency-wrapper'>
         <Currency/>
+        <AddCurrency/>
     </div>
   )
 }
